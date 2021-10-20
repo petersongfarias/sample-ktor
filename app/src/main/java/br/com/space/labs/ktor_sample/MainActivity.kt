b92d86aa8ca9463d25cb9b37050b7adeb6fe5138
Peterson Farias <petersongfarias@gmail.com>
2021-10-20 12:00:45 -0300

    add project

---
+++ app/src/main/java/br/com/space/labs/ktor_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.space.labs.ktor_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
